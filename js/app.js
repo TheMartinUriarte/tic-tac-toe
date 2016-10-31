809379f840f050c4f34815d8274e68aa20e2b7cb
TheMartinUriarte <themartinuriarte.programmer@gmail.com>
2016-10-30 23:14:12 -0700

    Added background image, new reset button, and into comments

--- js/app.js
+++ js/app.js
@@ -7,3 +7,14 @@ $(document).ready(function() {
 
 // alert("yes it does!!");
 //Pre-flight check
+
+//This is a weekend project assigned by the instructors at General Assembly's Web Development Immersive course in San Francisco.
+
+//This project is to have us, the students, create a Tic-Tac-Toe game with the use of Bootstrap and present it to the class on Monday 10/31/16.
+
+//Since I am currently unfamiliar with Bootstrap, you will find notations all throughout the code.
+
+//This is to serve as a point of reference for me when I return and am required to explain my work to my fellow classmates or instructors.
+
+//LEGEND:
+//DF = Default. The code given to me at the start of the assignment.
